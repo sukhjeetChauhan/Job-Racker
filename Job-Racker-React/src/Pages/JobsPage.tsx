dadec84d684b922e90aa6cf38e3df3cf51ac17e0
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-10-16 23:59:51 +1300

    add status change ability to job page

--- Job-Racker-React/src/Pages/JobsPage.tsx
+++ Job-Racker-React/src/Pages/JobsPage.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react'
-import { getAllJobs } from '../apis/jobApis'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { getAllJobs, updateStatus } from '../apis/jobApis'
 
-type Status = 'applied' | 'rejected' | 'interviewed' | 'offer'
+export type Status = 'applied' | 'rejected' | 'interviewed' | 'offer'
 
 type Jobs = {
   id: number
@@ -13,6 +13,9 @@ type Jobs = {
 
 export default function JobsPage() {
   const [jobs, setJobs] = useState<Jobs[] | undefined | null>(null)
+  const [filterOption, setFilterOption] = useState<string>('')
+  const [filterInput, setFilterInput] = useState<string>('')
+  const [refetch, setRefetch] = useState<boolean>(false)
   useEffect(() => {
     async function fetchData() {
       const allJobs = await getAllJobs()
@@ -20,7 +23,7 @@ export default function JobsPage() {
     }
 
     fetchData()
-  }, [])
+  }, [refetch])
 
   function convertDate(date: string) {
     const dateNow = new Date(date)
@@ -33,34 +36,112 @@ export default function JobsPage() {
     const nzFormattedDate = dateNow.toLocaleDateString('en-NZ', options)
     return nzFormattedDate
   }
+
+  function setStatusColor(status: string) {
+    const res =
+      status === 'applied'
+        ? 'bg-transparent'
+        : status === 'rejected'
+        ? 'bg-red-300'
+        : status === 'interviewed'
+        ? 'bg-yellow-300'
+        : 'bg-green-300'
+    return res
+  }
+
+  async function handleChange(
+    e: ChangeEvent<HTMLSelectElement>,
+    id: number,
+    status: string
+  ): Promise<void> {
+    const userConfirmed = window.confirm('Are you sure you want to proceed?')
+
+    if (userConfirmed) {
+      if (e.target.value !== status)
+        await updateStatus(id, e.target.value as Status)
+      setRefetch(!refetch)
+    }
+  }
+
   return (
-    <div className="w-4/5 mx-auto my-32 bg-gray-200 rounded min-h-[500px] p-8">
-      <table className="min-w-full table-auto border-collapse border border-gray-500">
-        <thead>
-          <th className="border border-gray-500 px-4 py-2">Job Title</th>
-          <th className="border border-gray-500 px-4 py-2">Company Name</th>
-          <th className="border border-gray-500 px-4 py-2">Status</th>
-          <th className="border border-gray-500 px-4 py-2">Date applied</th>
-        </thead>
-        <tbody>
-          {jobs?.map((job, i) => (
-            <tr key={i} className={i % 2 === 0 ? 'bg-blue-200' : 'bg-blue-100'}>
-              <td className="border border-gray-500 px-4 py-2 text-center">
-                {job.job_title}
-              </td>
-              <td className="border border-gray-500 px-4 py-2 text-center">
-                {job.company_name}
-              </td>
-              <td className="border border-gray-500 px-4 py-2 text-center">
-                {job.status}
-              </td>
-              <td className="border border-gray-500 px-4 py-2 text-center">
-                {convertDate(job.date_applied)}
-              </td>
+    <div className="flex flex-col">
+      <div className="w-4/5 mx-auto">
+        <div className="ml-auto w-fit mt-16 flex gap-2">
+          <p className="text-lg">Filter By:</p>
+          <select
+            value={filterOption}
+            className="border-2 border-gray-400 rounded bg-gray-100 p-[3px]"
+          >
+            <option value="job_title">Job Title</option>
+            <option value="company_name">Company Name</option>
+            <option value="date_applied">Date</option>
+          </select>
+          <input
+            value={filterInput}
+            onChange={(e) => setFilterInput(e.target.value)}
+            className="border-2 border-gray-400 rounded bg-gray-100 p-[3px]"
+          />
+          <button className="bg-blue-500 px-4 rounded text-white">
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="w-4/5 mx-auto mb-32 mt-16 bg-gray-200 rounded min-h-[500px] p-8">
+        <table className="min-w-full table-auto border-collapse border border-gray-500">
+          <thead>
+            <tr>
+              <th className="border border-gray-500 px-4 py-2">Job Title</th>
+              <th className="border border-gray-500 px-4 py-2">Company Name</th>
+              <th className="border border-gray-500 px-4 py-2">Status</th>
+              <th className="border border-gray-500 px-4 py-2">Date applied</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {jobs
+              ?.sort(
+                (a, b) =>
+                  new Date(b.date_applied).getTime() -
+                  new Date(a.date_applied).getTime()
+              )
+              .map((job, i) => (
+                <tr
+                  key={i}
+                  className={i % 2 === 0 ? 'bg-blue-200' : 'bg-blue-100'}
+                >
+                  <td className="border border-gray-500 px-4 py-2 text-center">
+                    {job.job_title}
+                  </td>
+                  <td className="border border-gray-500 px-4 py-2 text-center">
+                    {job.company_name}
+                  </td>
+                  <td
+                    className={`${setStatusColor(
+                      job.status
+                    )} border border-gray-500 px-4 py-2 text-center flex justify-between gap-2 relative`}
+                  >
+                    <select
+                      className=" bg-transparent w-full focus:outline-none capitalize"
+                      value={job.status}
+                      onChange={(e) => handleChange(e, job.id, job.status)}
+                    >
+                      <option value={job.status}>{job.status}</option>
+                      <option>----------</option>
+                      {job.status !== 'applied' && (
+                        <option value="applied">Applied</option>
+                      )}
+                      <option value="rejected">Rejected</option>
+                      <option value="interviewed">Interviewed</option>
+                      <option value="offer">Offer</option>
+                    </select>
+                  </td>
+                  <td className="border border-gray-500 px-4 py-2 text-center">
+                    {convertDate(job.date_applied)}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- Job-Racker-React/src/apis/jobApis.ts
+++ Job-Racker-React/src/apis/jobApis.ts
@@ -1,9 +1,21 @@
 import axios from 'axios'
+import { Status } from '../Pages/JobsPage'
 
 export async function getAllJobs() {
   try {
     const response = await axios.get('http://localhost:8000/api/apply/')
     return response.data
+  } catch (error) {
+    console.log(`Response: ${error}`)
+  }
+}
+
+export async function updateStatus(id: number, newStatus: Status) {
+  try {
+    const response = await axios.put(`http://localhost:8000/api/apply/${id}/`, {
+      status: newStatus,
+    })
+    console.log(response.data)
   } catch (error) {
     console.log(`Response: ${error}`)
   }
